7230231b9449c02afbadedc4397164a142a12d82
Ezell <lynellf@gmail.com>
2021-07-04 12:47:48 -0500

    got it functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { useRTCRecorder } from "./useRTC";
 
 function App() {
+  const { state, error, handleStart, handleStop } = useRTCRecorder();
+  if (state === "error") {
+    return (
+      <main>
+        <h1>{error?.message}</h1>
+      </main>
+    );
+  }
   return (
     <main className="App">
       <section>
-        <button>Start Recording</button>
-        <button>Stop Recording</button>
+        <button onClick={handleStart}>Start Recording</button>
+        <button onClick={() => handleStop()}>Stop Recording</button>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/useRTC.ts
+++ src/useRTC.ts
@@ -1,67 +1,121 @@
-import { useState } from "react"
-import type {MutableRefObject} from 'react'
-import { useMediaStream } from "./useMediaStream"
+import { useState, useEffect } from "react";
+import { useMediaStream } from "./useMediaStream";
 
-const INITIAL_CTX = {
-  state: '',
-  recoder: null,
-  error: null
-}
+type TIsRecording = "recording";
+type TNotRecording = "notRecording";
+type TError = "error";
+type TCTX = {
+  state: TIsRecording | TNotRecording | TError;
+  recorder: TRecorder | null;
+  error: Error | null;
+};
+const INITIAL_CTX: TCTX = {
+  state: "notRecording",
+  recorder: null,
+  error: null,
+};
 
-async function getRTC() {
-  const mod = await import("recordrtc")
-  return mod.default
+type TStream = MediaStream | void;
+async function getRecorder(stream: TStream) {
+  if (!stream) throw new Error("No Media Stream!");
+  const mod = await import("recordrtc");
+  const recorderType = mod.GifRecorder;
+  return [stream, mod.default, recorderType] as const;
 }
-type TStream = MediaStream | null
 
 /**
- * typedef from S/O by Jon Jaques
+ * How to unwrap a promise via S/O by Jon Jaques
  * https://stackoverflow.com/questions/48011353/how-to-unwrap-type-of-a-promise
  */
 type Await<T> = T extends {
-  then(onfulfilled?: (value: infer U) => unknown): unknown
+  then(onfulfilled?: (value: infer U) => unknown): unknown;
 }
   ? U
-  : T
+  : T;
 
-type TRTC = Await<ReturnType<typeof getRTC>>
-function getRecorder(RecordRTC: TRTC) {
-  return async (stream: TStream) =>
-    stream
-      ? new RecordRTC(stream, {
-          type: "gif",
-          frameRate: 60,
-          bitrate: 128000,
-        })
-      : null
+type TRecordRTC = Await<ReturnType<typeof getRecorder>>;
+async function initRecorder([stream, RecordRTC]: TRecordRTC) {
+  return new RecordRTC(stream, {
+    type: "video",
+  });
 }
 
-type TRecorder = Await<ReturnType<ReturnType<typeof getRecorder>>>
-type TRecorderRef = MutableRefObject<TRecorder>
+type TRecorder = Await<ReturnType<typeof initRecorder>>;
+type TDispatch = React.Dispatch<React.SetStateAction<TCTX>>;
 
-function getRecorderSetter(recorderRef: TRecorderRef) {
+function getRecorderSetter(dispatch: TDispatch) {
   return async (recorder: TRecorder) => {
-    recorderRef.current = recorder
-    return recorder
-  
+    dispatch({ state: "recording", recorder, error: null });
+    return recorder;
+  };
 }
 
-async function startRecording(recorder: TRecorder){
-  recorder?.startRecording()
-  return recorder
+function getErrorSetter(dispatch: TDispatch) {
+  return (error: Error) => dispatch({ state: "error", recorder: null, error });
 }
-type TPromptUser = ReturnType<typeof useMediaStream>[1]
-type TSetRecorder = ReturnType<typeof getRecorderSetter>
+
+async function startRecording(recorder: TRecorder) {
+  recorder.startRecording();
+  return recorder;
+}
+
+type TPromptUser = ReturnType<typeof useMediaStream>[1];
+type TSetRecorder = ReturnType<typeof getRecorderSetter>;
+type TSetError = ReturnType<typeof getErrorSetter>;
 function getStartHandler(setRecorder: TSetRecorder) {
-  return (promptUser: TPromptUser) => () => {
-    getRTC()
-  }
+  return (setError: TSetError) => (promptUser: TPromptUser) => () => {
+    promptUser()
+      .then(getRecorder)
+      .then(initRecorder)
+      .then(setRecorder)
+      .then(startRecording)
+      .catch(setError);
+  };
 }
 
+function getSaveBlob(recorder: TRecorder | null) {
+  return (filename = "recording.webm") =>
+    async () => {
+      const mod = await import("save-as");
+      const saveAs = mod.saveAs;
+      const blob = recorder?.getBlob();
+      saveAs(blob, filename);
+    };
+}
 
-export function useRTC() {
-  const [{ state: streamState, error: streamError, stream }, promptUser] =
-    useMediaStream()
-  const [] = useState()
-  
+function getStopHandler(recorder: TRecorder | null) {
+  return (filename = "recording.webm") => {
+    const saveBlob = getSaveBlob(recorder)(filename);
+    recorder?.stopRecording(saveBlob);
+  };
+}
+
+export function useRTCRecorder() {
+  const [mediaStreamCtx, promptUser] = useMediaStream();
+  const [recorderCtx, dispatch] = useState(INITIAL_CTX);
+  const { state, recorder, error } = recorderCtx;
+
+  const setRecorder = getRecorderSetter(dispatch);
+  const setError = getErrorSetter(dispatch);
+  const handleStart = getStartHandler(setRecorder)(setError)(promptUser);
+  const handleStop = getStopHandler(recorder);
+  const { state: streamState, error: streamError } = mediaStreamCtx;
+
+  // Bubble up any errors from useMediaStream()
+  useEffect(() => {
+    if (streamState === "error") {
+      setError(streamError as Error);
+    }
+
+    if (state === "error") {
+      console.error(error);
+    }
+  }, [streamState, streamError, state, error]);
+
+  return {
+    state,
+    error,
+    handleStart,
+    handleStop,
+  };
 }
