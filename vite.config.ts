bf84f7a0a30d88551ad0991fc0344e478ee5815f
Ezell <lynellf@gmail.com>
2021-07-11 11:45:59 -0500

    add cross origin plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), crossOriginIsolation()]
 })
